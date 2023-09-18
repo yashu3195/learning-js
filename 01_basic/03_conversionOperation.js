88cfc02ade800a4850d7c1a90b99f29f9e6a17a0
yashu3195 <145327314+yashu3195@users.noreply.github.com>
2023-09-18 12:36:49 +0000

    conversion_operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -25,5 +25,45 @@ let BooleanisLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+                /*****                  Operations                ******/
+
+                let value = 3;
+                let negValue = -value;
+                //console.log(negValue)
+
+                //simple operations
+                /*console.log(2+2)
+                /*console.log(2/2)
+                /*console.log(2*2)
+                /*console.log(2-2)
+                /*console.log(2%3)*/
+
+                let str1 = "Hello"
+                let str2 = " Anubhav"
+                let str3 = str1 + str2
+                //console.log(str3)
+
+               /* console.log("1" + 2)
+                console.log(1 + "2")
+                console.log("1" + "2")
+                console.log(2 + 1 + "5")*/
+                //console.log(true)
+              //  console.log(+true)
+                //console.log(true+)
+                
+               
+                /*let num1, num2, num3
+                num1 = num2 = num3 = 2+2; 
+
+                let gameCounter = 100;
+                gameCounter++;
+                console.log(gameCounter)*/
+               
+                let gameCounter = 100;
+                ++gameCounter;
+                console.log(gameCounter)
+               
