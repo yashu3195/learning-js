24a4fff21e37de6f7ecbfd196f8631b98f5dfb67
yashu3195 <145327314+yashu3195@users.noreply.github.com>
2023-09-18 11:52:02 +0000

    conversion-of-datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true;
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//conversion
+/*   "33" => 33
+33abc  =>  NaN ( not a number)
+True => 1        False => 0  */
+
+
+let isLoggedIn = ""
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(BooleanisLoggedIn)
+
+// true => 1,    False  => 0
+// 1  => true
+// "" => False
+// "yashu"    => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
