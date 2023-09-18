00c20b051ec73cdf9bfc8bf09bfedcaf42e6076b
yashu3195 <145327314+yashu3195@users.noreply.github.com>
2023-09-18 14:58:40 +0000

    comparison of data and data types

---
+++ 01_basic/04_comparisonOp.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1)
+
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null == 0);   
+
+//avoid these type (line 10 to 19) comparison- clean code is the priorit
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+//     ===
+
+console.log("2" === 2);
