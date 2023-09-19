aa34a771ba5e2053ce579c5b975a9c1b9f885f76
yashu3195 <145327314+yashu3195@users.noreply.github.com>
2023-09-19 08:56:31 +0000

    memories, heap and stack

--- 01_basic/05_datatypes-summary.js
+++ 01_basic/05_datatypes-summary.js
@@ -6,7 +6,7 @@
 
 
 
-const score = 100;
+/*const score = 100;
 const scoreValue = 100.3;
 
 const isLoggedIn = false;
@@ -39,4 +39,35 @@ console.log(typeof id);
 console.log(typeof myFunction);
 console.log(typeof isLoggedIn);
 console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
+console.log(typeof outsideTemp);*/
+
+//*******************************************************************/
+                  //memory
+
+    // primitive => stack
+    // reference/non-primitive  => heap
+
+
+
+
+
+
+
+let myYoutubename = "literaturelane"
+let anothername = myYoutubename
+anothername = "code and learn"
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "anubhav@google.com",
+    upi: "anubhav@ybl"
+}
+
+let userTwo = userOne;
+userTwo.email = "vibhav@gmail.com";
+
+console.log(userOne);
+console.log(userTwo);
+
+
