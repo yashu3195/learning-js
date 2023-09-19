64e1a161a4d7e11b1f7e1d4bacf960fbeb77c686
yashu3195 <145327314+yashu3195@users.noreply.github.com>
2023-09-19 18:25:40 +0000

    number and maths

---
+++ 01_basic/07_number_and_maths.js
@@ -0,0 +1,50 @@
+// *********************Numbers*******************
+
+
+
+/*const score = 400;
+console.log(score);
+
+const newScore = new Number(100);
+console.log(newScore);
+
+console.log(newScore.toString());
+console.log(newScore.toString().length);
+console.log(newScore.toFixed(2));
+
+const otherNumber = 23.8699;
+
+console.log(otherNumber.toPrecision(3));
+
+const anotherNumber = 123.8699;
+
+console.log(anotherNumber.toPrecision(4));
+const newValue = 1000000;
+console.log(newValue.toLocaleString('en-In'));
+console.log(newValue.toLocaleString()); */
+
+
+
+
+//********************  Maths ***************/
+
+//console.log(Math.abs(-4));
+//console.log(Math.abs(4));
+//console.log(Math.round(4.3));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.1));
+//console.log(Math.sqrt(4));
+//console.log(Math.min(4, 2, 9, 56, 420));
+//console.log(Math.max(4, 120, 556, 2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
