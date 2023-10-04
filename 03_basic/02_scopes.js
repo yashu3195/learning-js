ac70110319a64a54324cf37790b4c016e921b54c
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-04 09:46:49 +0530

    block scope and global scope

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,16 @@
+//let A = 10
+//const B = 20
+//var C = 30
+//console.log(A);
+//console.log(B);
+//console.log(C);
+const  A = 56
+
+if (true) {
+    let A = 10
+    const B = 20
+    //var C = 30      //avoid var - ye global variable hi rahega scope me bhi
+    console.log(A)        //output = 10
+}
+     
+console.log(A);       // output = 56
