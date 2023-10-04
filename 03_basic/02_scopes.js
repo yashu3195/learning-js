7218f267a666b539c3f8f07485141ced446fed2d
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-04 11:09:18 +0530

    hoisting

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -4,7 +4,7 @@
 //console.log(A);
 //console.log(B);
 //console.log(C);
-const  A = 56
+/*const  A = 56
 
 if (true) {
     let A = 10
@@ -13,4 +13,73 @@ if (true) {
     console.log(A)        //output = 10
 }
      
-console.log(A);       // output = 56
+console.log(A);       // output = 56 */
+
+/*function one(){
+
+    const userName = "Hitesh"
+
+    function two() {
+
+        const website = "Google"
+
+        console.log(userName);
+    }
+        //console.log(website);
+
+    two()
+
+
+}
+
+one() */
+
+
+/*function main() {
+
+    const username = "Yashu"
+
+    function subMain() {
+
+        const website = "youtube"
+
+        console.log(username);
+        
+    }
+    subMain()
+    
+    //console.log(website);
+}
+
+main()
+
+*/
+
+
+
+if (true){
+
+    const user = "Shruti";
+
+    if(user === "Shruti"){
+        const websiTe = " Meta"
+        //console.log(user +  websiTe);
+    }
+    //console.log(websiTe);    --first error--website is not defined
+
+}
+//console.log(user);    --second errod--user is not defined
+
+console.log(addOne(5))
+// is tareeke se likhne par initialization se pahle bhi value access kar sakte hai
+function addOne(num){
+    return num + 1
+}
+
+//console.log(addTwo);
+const addTwo = function(num){
+    return num + 2
+ 
+     //is tareeke se likhne par initialization se pahle value access nahi kar sakte
+}
+console.log(addTwo(8));
