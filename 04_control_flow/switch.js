b79f66d1b56f4d9fe00a5e0f7f18a94958c37342
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-05 17:48:22 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,50 @@
+
+
+//const month = 3;
+//
+//switch (month) {
+//    case 1:
+//        console.log("january");
+//        break;
+//
+//        case 2:
+//            console.log("february");
+//            break;
+//                                                   // number data type
+//            case 3:
+//                console.log("March");
+//                break;
+//
+//                case 4:
+//                 console.log("April");
+//                 break;
+//
+//        default:
+//        console.log("default case match");
+//        break;
+//}
+
+
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+
+        case "feb":
+            console.log("february");
+            break;
+                                                 //string data type
+            case "march":
+                console.log("March");
+                break;
+
+                case "april":
+                 console.log("April");
+                 break;
+
+        default:
+        console.log("default case match");
+        break;
+}
