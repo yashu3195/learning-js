5b95e1269e2537e45f703138de45a07aae270ad5
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-05 23:13:57 +0530

    truthy and falsly

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,63 @@
+/*const username = "Anubhav"
+
+
+
+if (username) {
+    console.log("You are Permitted to enter");
+} else {
+    console.log("enter right username");
+}*/
+
+
+//const username = ""
+//
+//
+//
+//if (username) {
+//    console.log("You are Permitted to enter");
+//} else {
+//    console.log("enter right username");
+//}
+
+//    FALSY VALUES
+//      false, 0, -0, BigInt 0n, "empty string", null, undefined, Nan
+
+//      TRUTHY VALUES
+
+//    "0", "false", "  ", {}, [], function(){}
+
+
+//const myArray = []
+//
+//if (myArray.length == 0) {
+//    console.log("array is empty");
+//}
+
+//const myObj = {}
+
+//if (Object.keys(myObj).length === 0) {
+   // console.log("object is empty");
+//}
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+//let val1;
+// val1 = 5 ?? 10
+ //val1 = null ?? 10
+ //val1 = undefined ?? 15
+//val1 = null ?? 10 ?? 20
+
+
+
+//console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+//const iceTeaPrice = 100
+//iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+const passingMarks = 30
+passingMarks >= 33 ? console.log("You are Passed This Quiz") : console.log("You are Failed This Quiz- lol")
