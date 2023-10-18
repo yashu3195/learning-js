f194c813ba7f5c013c650b8ce0c160ad9be8643b
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-18 13:26:19 +0530

    loops do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//let index = 0;
+//while (index <=10 ) {
+//    //console.log(`The Value is: ${index}`);
+//    index = index + 2;
+//}
+
+let myArray = ['flash', 'batman', 'spider man', 'doga']
+
+let arr = 0
+
+while (arr < myArray.length) {
+   // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 1
+
+do {
+    //console.log(`score is: ${score}`);
+    score = score + 1
+
+} while (score <= 10);
+
+let score1 = 11
+
+do {
+    console.log(score1);
+}
+while(score1 <= 10)
