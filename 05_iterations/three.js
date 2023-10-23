a6e7d39127d878db052a0d469b827e06b7992f61
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-23 12:02:13 +0530

    map , for of loop, iterator of object

---
+++ 05_iterations/three.js
@@ -0,0 +1,47 @@
+// for off
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+/*const arr = [1,2,3,4,5]
+
+for (const number of arr) {
+    //console.log(number);
+    
+}
+
+const greet = "Hello World!"
+
+for (const greeting of greet){
+console.log(`each char is ${greeting}`);}
+
+
+//    Map
+
+
+
+const map = new Map()
+
+map.set("USA", "United States of America")
+map.set("IND", "India")
+map.set("FRA", "France")
+map.set("PAK", "Pakistan")f
+
+console.log(Map);
+
+for (const [key, value] of map) {
+    console.log(key, ":-", value);
+}
+*/
+
+
+const myObj = {
+    game1: 'NFS',
+    game2: 'GTA'
+}
+
+
+for (const game of myObj) {
+    console.log(game);
+}
+
