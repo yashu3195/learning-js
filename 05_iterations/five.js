f523f05615a3a148a9a8b2c25f03ec2ea227f3ce
yashu3195 <anubhavsingh.ce@gmail.com>
2023-10-24 15:26:08 +0530

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+//coding.forEach (function (val){
+//console.log(val);
+//})
+
+//coding.forEach(  (item) => {
+//    console.log(item);
+//})
+
+//function printMe(item) {
+//    console.log(item);
+//}
+//
+//coding.forEach(printMe)
+
+//coding.forEach((item, index, arr) => {
+//console.log(item, index, arr);
+//})
+
+const myCoding = [
+    {
+        launguageName: "javascript",
+        languageFileNameame: "js"
+    },
+    {
+        launguageName: "java",
+        languageFileNameame: "java"
+    },
+    {
+        launguageName: "Python",
+        languageFileNameame: "Py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.launguageName);
+})
